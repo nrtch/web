bcc350704f9e13e55afacdfc084c978a769b6e8f
hssrrw <hissorrow@gmail.com>
2019-03-22 15:46:45 +0200

    fix android detection

--- helpers/utils.js
+++ helpers/utils.js
@@ -7,7 +7,8 @@ export const platform =
   typeof navigator !== 'undefined'
     ? /^iP(hone|od|ad)/.test(navigator.platform)
       ? 'ios'
-      : /^Android/.test(navigator.platform)
+      : /^Android/.test(navigator.platform) ||
+        /\sAndroid\s/.test(navigator.userAgent)
       ? 'android'
       : null
     : null;
